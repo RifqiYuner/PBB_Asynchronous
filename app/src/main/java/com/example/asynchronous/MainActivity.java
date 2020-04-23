2ef4880bbd4b28cfe0cd721b809058dd2d1b8bf3
ColdDeus <31419775+ColdDeus@users.noreply.github.com>
2020-04-23 13:27:30 +0700

    Tugas PBB 23/4/2020

---
+++ app/src/main/java/com/example/asynchronous/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.asynchronous;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TEXT_STATE = "currentText";
+    private static final String TEXT_PROGRESS = "CurrentSleepDuration";
+    private TextView mTextView = null;
+    private TextView mProgressTextView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView1);
+        mProgressTextView = findViewById(R.id.textViewResult);
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+            mProgressTextView.setText(savedInstanceState.getString(TEXT_PROGRESS));
+        }
+    }
+
+
+    public void startTask (View view) {
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView, mProgressTextView).execute();
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+        outState.putString(TEXT_PROGRESS, mProgressTextView.getText().toString());
+    }
+}
---
+++ app/src/main/java/com/example/asynchronous/SimpleAsyncTask.java
@@ -0,0 +1,56 @@
+package com.example.asynchronous;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+    private WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mResultTextView;
+
+    public SimpleAsyncTask(TextView tv, TextView result) {
+        mTextView = new WeakReference<>(tv);
+        mResultTextView = new WeakReference<>(result);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        int s = n * 200;
+        publishProgress(s,n); //akan mengupdate ui ketika waktu sleep telah dihitung
+        Log.d(TAG, "doInBackground: " + s);
+
+
+
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        mResultTextView.get().setText("Current sleep time: " + values[0] + " ms"); //memunculkan ui update yang memberitahu berapa lama thread itu tidur
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+
+
+
+}
